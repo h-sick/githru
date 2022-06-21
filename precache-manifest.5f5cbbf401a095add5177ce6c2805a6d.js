2a40686bd493a755eb4a4029597cfa78b5ec2f3b
h-sick <luna23825@gmail.com>
2022-06-21 19:19:21 +0900

    Updates

---
+++ precache-manifest.5f5cbbf401a095add5177ce6c2805a6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b7ede2605e4e95d5bd310001428e57",
+    "url": "/githru/index.html"
+  },
+  {
+    "revision": "d1e0f2842b6328ec6858",
+    "url": "/githru/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "892cc82796a902974cdd",
+    "url": "/githru/static/css/main.b3dd60eb.chunk.css"
+  },
+  {
+    "revision": "d1e0f2842b6328ec6858",
+    "url": "/githru/static/js/2.06c76fdc.chunk.js"
+  },
+  {
+    "revision": "ddb78e018dea340759d4e2783902d6e2",
+    "url": "/githru/static/js/2.06c76fdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "892cc82796a902974cdd",
+    "url": "/githru/static/js/main.5ec65c09.chunk.js"
+  },
+  {
+    "revision": "fe204c4d30d834692f1b",
+    "url": "/githru/static/js/runtime-main.a7ba3d90.js"
+  }
+]);
